698dd1876fe987e9b4245060c172bb2a6f7acfa6
abdulrawoofali <rawoofabd41@gmail.com>
2020-12-05 18:11:19 +0530

    corrected

--- src/index.js
+++ src/index.js
@@ -1,34 +1,30 @@
-const express = require('express')
-const {newsArticleModel} = require('./connector')
-const app = express()
-const port = 8080
+const express = require("express");
+const { newsArticleModel } = require("./connector");
+const app = express();
+const port = 8080;
 
-const onePageArticleCount = 10
-
-
-
+const onePageArticleCount = 10;
 
 // Parse JSON bodies (as sent by API clients)
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.get("/newFeeds/:limit/:offset", (req, res) => {
-    let limit = parseInt(req.body.limit) ;
-    let offset =  parseInt(req.body.offset) ;
-    if(isNaN(req.body.limit)||isNaN(req.body.offset)){
-        limit = 10;
-        offset =0;
-    }
-    newsArticleModel
+  let limit = parseInt(req.params.limit);
+  let offset = parseInt(req.params.offset);
+  if (isNaN(req.params.limit) || isNaN(req.params.offset)) {
+    limit = 10;
+    offset = 0;
+  }
+  //console.log(limit,offset);
+  newsArticleModel
     .find()
-    .skip(parseInt(req.params.offset))
-    .limit(parseInt(req.params.limit))
-    .then((news) => res.send(news))
+    .limit(limit)
+    .skip(offset)
+    .then((news) => res.send({ length: news.length }))
     .catch((err) => console.log(err));
 });
 
-
-
-app.listen(port, () => console.log(`App listening on port ${port}!`))
+app.listen(port, () => console.log(`App listening on port ${port}!`));
 
 module.exports = app;
